2c33f305a6c4890af2321fffe4bf26314ea4c2e3
Francisco Reyes <franciscoreyesgutierrez@gmail.com>
2013-07-30 11:12:06 -0500

    Programa que busca de alguna manera el UID proximo disponible

--- next_uid.sh
+++ next_uid.sh
@@ -3,7 +3,7 @@
 #
 # Programado por: Francisco Reyes Gutierrez
 # Fecha: 05 de junio de 2013
-#
+
 awk -F":" '{ print $3 }' /etc/passwd > number_list
 awk -F":" '{ print $3 }' /etc/group >> number_list
 A=` sort -g number_list | tail -1`
